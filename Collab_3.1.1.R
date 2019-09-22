c1505c192c011c5c747fd293b8f06f083ed78acc
Kyle Nguyen <kyle.nguyen@Kyles-MacBook-Pro.local>
2019-09-22 13:25:25 +0800

    Kyle fix missing variable n

--- Collab_3.1.1.R
+++ Collab_3.1.1.R
@@ -1,4 +1,5 @@
-data <- read.csv(file = "JPM.csv")
+data <- read.csv(file = "./JPM.csv")
+n <- length(data$Adj.Close)
 avg_price <- mean(data$Adj.Close)
 daily_return <- (data$Adj.Close[(2:n)] - data$Adj.Close[(1:n-1)])/(data$Adj.Close[(2:n)])
 volatility <- sd(daily_return)
